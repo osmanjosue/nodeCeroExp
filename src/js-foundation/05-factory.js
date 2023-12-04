2e54259f97186835a91f0c86cfa283c0bbf4afcf
osmanjosue <osmanjosue@gmail.com>
2023-12-04 09:04:33 -0600

    Curso node cero a experto => Factory Functions = Necesidad

--- src/js-foundation/05-factory.js
+++ src/js-foundation/05-factory.js
@@ -1,9 +1,12 @@
+const {v4: uuidv4} = require('uuid');
+const getAge = require( 'get-age');
+
 const buildPerson = ({name, birthdate}) => {
     return {
-        id: new Date().getTime(),
+        id: uuidv4(),
         name: name,
         birthdate: birthdate,
-        age: new Date().getFullYear() - new Date(birthdate). getFullYear(),
+        age: getAge(birthdate),
     }
 }
 
