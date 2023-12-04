11e8e6baf54812a3d85daf12b4e12b4e66c8ada8
osmanjosue <osmanjosue@gmail.com>
2023-12-04 08:24:54 -0600

    Curso node cero a experto => Arrow functions

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 // const {emailTemplate} = require ('./js-foundation/01-template')
 // require ('./js-foundation/02-destructuring')
-const {getUserById} = require( './js-foundation/03-callbacks');
+// const {getUserById} = require( './js-foundation/03-callbacks');
+const {getUserById} = require( './js-foundation/04-arrow');
 
 // console.log(emailTemplate);
 
 id=1;
-getUserById( id, function(error, user){
-    if(error){
-        throw new Error( error );
-    }
+getUserById( id, (error, user) => {
+    if(error)throw new Error( error );    
 
-    getUserById(2, function(error, user2){
+    getUserById(2, (error, user2) => {
         if (error) {
             throw new Error(error);
         }
