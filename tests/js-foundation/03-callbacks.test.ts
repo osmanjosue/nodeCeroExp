1bd3303a6bc7159389191dfe58e24e0082da7aa2
osmanjosue <osmanjosue@gmail.com>
2024-04-06 13:18:56 -0600

    Seccion 5 => Clase: Pruebas en 03-callbacks-Exito

--- tests/js-foundation/03-callbacks.test.ts
+++ tests/js-foundation/03-callbacks.test.ts
@@ -1,11 +1,21 @@
 import { getUserById } from '../../src/js-foundation/03-callbacks';
 
 describe('js-foundation/03-callbacks.ts', () => { 
-        test('GetUserById should return an error if user does not exists', () => { 
+        test('GetUserById should return an error if user does not exists', (done) => { 
             const id = 10;
             getUserById(id, (err,user)=> {
                 expect( err ).toBe(`User not found with id ${id}`);
                 expect(user).toBeUndefined();
+                done();
             });
-         })
+         });
+
+         test('GetUserById should return john doe when user id is 1', () => { 
+            const id = 1;
+            getUserById(id, (err, user)=> {
+                expect(err).toBeUndefined();
+                expect( user ).toEqual({id: 1, name: 'John Doe'});
+
+            })
+          })
  })
