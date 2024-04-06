808fdf9707b44f5062fbf535c74551f21a7bb044
osmanjosue <osmanjosue@gmail.com>
2024-04-05 21:29:48 -0600

    Seccion 5 => Clase: Arrange, Act y Assert

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,16 @@
-describe('App', () => {
+describe('Test in the App File', () => {
     test('should be true', () => {
-        expect(true).toBe(true);
+
+        //1. Arrange
+        const num1 = 10;
+        const num2 = 20;
+
+        //2. Act
+        const result = num1 + num2;
+
+        //3. Assert
+        expect(result).toBe(30);
+
+
     })
 })
