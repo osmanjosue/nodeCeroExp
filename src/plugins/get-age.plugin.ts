984338d05d1b71175a3dc48d368c2681bd2c3fb0
osmanjosue <osmanjosue@gmail.com>
2024-04-06 16:51:08 -0600

    Seccion 5 => Clases: Pruebas en getAgeAdapter & SpyOn - Metodos de objetos

--- src/plugins/get-age.plugin.ts
+++ src/plugins/get-age.plugin.ts
@@ -1,6 +1,9 @@
 
 export const getAge = (birthdate: string) => {
-    if(!birthdate) return new Error( 'birthdate is required' );
 
-    return new Date().getFullYear() - new Date (birthdate).getFullYear();
+    console.log({ currentYear: new Date().getFullYear() })
+
+    if (!birthdate) return new Error('birthdate is required');
+
+    return new Date().getFullYear() - new Date(birthdate).getFullYear();
 }
